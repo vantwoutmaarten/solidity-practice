6c6a96e52c02ca4407d98e708f5686c111929b20
vantwoutmaarten <maarten@vantwout.net>
2021-12-30 20:12:27 +0100

    created the most minimalistic contract to deploy NFT's

--- project6-NFT-dogs-with-random-scarcity-features/contracts/SimpleCollectible.sol
+++ project6-NFT-dogs-with-random-scarcity-features/contracts/SimpleCollectible.sol
@@ -1,13 +1,19 @@
- // SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
+    uint256 tokenCounter;
 
-    constructor () public ERC721 ("Dogie", "DOG") {
-        
+    constructor() public ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
     }
 
+    function createCollectible() public returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
 }
