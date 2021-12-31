4c30d525d19efdee2cfedae4d9fd43e2e27bf382
vantwoutmaarten <maarten@vantwout.net>
2021-12-31 11:20:32 +0100

    adorable Dog NFT hosted on IPFS now visible on OpenSea, rinkeby

--- project6-NFT-dogs-with-random-scarcity-features/contracts/SimpleCollectible.sol
+++ project6-NFT-dogs-with-random-scarcity-features/contracts/SimpleCollectible.sol
@@ -2,17 +2,22 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract SimpleCollectible is ERC721 {
-    uint256 tokenCounter;
+contract SimpleCollectible is ERC721URIStorage {
+    uint256 public tokenCounter;
 
     constructor() public ERC721("Dogie", "DOG") {
         tokenCounter = 0;
     }
 
-    function createCollectible() public returns (uint256) {
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
         uint256 newTokenId = tokenCounter;
         _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
         tokenCounter = tokenCounter + 1;
         return newTokenId;
     }
---
+++ project6-NFT-dogs-with-random-scarcity-features/scripts/deploy_and_create.py
@@ -0,0 +1,18 @@
+from scripts.helpful_scripts import get_account
+from brownie import SimpleCollectible
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
+
+
+def main():
+    account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+
+    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+    tx.wait(1)
+    print(
+        f"Awesome, now NFT visible on OpenSea at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() -1)}"
+    )
+
+    print("OpenSea will update in around 20 minutes")
