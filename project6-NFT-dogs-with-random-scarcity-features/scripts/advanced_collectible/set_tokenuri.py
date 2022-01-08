67487b862fb3afa1de53cb9f30f9626ba74cfdc7
vantwoutmaarten <maarten@vantwout.net>
2022-01-08 12:04:05 +0100

    created a dictionary for creatd tokenids and metadata

--- project6-NFT-dogs-with-random-scarcity-features/scripts/advanced_collectible/set_tokenuri.py
+++ project6-NFT-dogs-with-random-scarcity-features/scripts/advanced_collectible/set_tokenuri.py
@@ -1,6 +1,16 @@
 from brownie import network, AdvancedCollectible
 from scripts.helpful_scripts import OPENSEA_URL, get_account, get_breed
 
+tokenId_metadata_dic = {
+    0: "https://ipfs.io/ipfs/QmUQjup5E5KAESEFmNQmc9c5BtGA4Z1vpeY4b5VzhA73Ne?filename=0-ST_BERNARD.json",
+    1: "https://ipfs.io/ipfs/QmUQjup5E5KAESEFmNQmc9c5BtGA4Z1vpeY4b5VzhA73Ne?filename=1-ST_BERNARD.json",
+    2: "https://ipfs.io/ipfs/QmdJHLooxHfbzk6ZsVFrpyq1HQpcQSb97T1YC6HaJxZwCQ?filename=2-SHIBA_INU.json",
+    3: "https://ipfs.io/ipfs/QmUQjup5E5KAESEFmNQmc9c5BtGA4Z1vpeY4b5VzhA73Ne?filename=3-ST_BERNARD.json",
+    4: "https://ipfs.io/ipfs/QmdJHLooxHfbzk6ZsVFrpyq1HQpcQSb97T1YC6HaJxZwCQ?filename=4-SHIBA_INU.json",
+    5: "https://ipfs.io/ipfs/QmeYmgiRSE3VJFE51BRNgMz4SAAGQC3AU5111zcn1GR2oy?filename=5-HAPPY_PUG.json",
+}
+
+
 def main():
     print(f"working on {network.show_active()}")
     advanced_collectible = AdvancedCollectible[-1]
@@ -9,9 +19,10 @@ def main():
 
     for token_id in range(number_of_collectibles):
         breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
-        if not advanced_collectible.tokenURI(token_id).startwith("http")
+        if not advanced_collectible.tokenURI(token_id).startswith("http"):
             print(f"Set tokenURI of {token_id}")
-            set_tokenURI(token_id, nft_contract, tokenURI)
+            set_tokenURI(token_id, advanced_collectible, tokenId_metadata_dic[token_id])
+
 
 def set_tokenURI(token_id, nft_contract, tokenURI):
     account = get_account()
@@ -20,4 +31,3 @@ def set_tokenURI(token_id, nft_contract, tokenURI):
     print(
         f"Awesome! view NFT now at opensea: {OPENSEA_URL.format(nft_contract.address, token_id)}"
     )
-    
