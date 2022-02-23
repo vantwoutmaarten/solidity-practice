8b9b58cb34d4af361404aee65048b5dd03241782
vantwoutmaarten <maarten@vantwout.net>
2022-02-23 17:17:00 +0100

    build a Counter contract, used typechain to get solidity contract types, used ethers to deploy, used hardhat,waffle and mocha to test the contract

---
+++ typescript-practice/turtorial/hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-deploy';
+import '@nomiclabs/hardhat-etherscan'
+
+require('dotenv').config()
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [{ version: "0.8.12", settings: {} }],
+  },
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY? process.env.RINKEBY_PRIVATE_KEY : "0x000"],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+export default config;
