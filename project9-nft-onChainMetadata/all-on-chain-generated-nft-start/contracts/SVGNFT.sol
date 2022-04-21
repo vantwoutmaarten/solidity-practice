05e206ba2741adcffec47b5d126c9c861aa89389
vantwoutmaarten <maarten@vantwout.net>
2022-04-21 22:02:32 +0200

    create function to mint, and create image- and metadata and store on chain

--- project9-nft-onChainMetadata/all-on-chain-generated-nft-start/contracts/SVGNFT.sol
+++ project9-nft-onChainMetadata/all-on-chain-generated-nft-start/contracts/SVGNFT.sol
@@ -6,11 +6,47 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-
-// import "@openzeppelin/contracts/drafts/Counters.sol";
+import "base64-sol/base64.sol";
 
 contract SVGNFT is ERC721URIStorage {
-    constructor() public {
-        super();
+    uint256 public tokenCounter;
+
+    constructor() ERC721("SVG NFT", "svgNFT") {
+        tokenCounter = 0;
+    }
+
+    function create(string memory svg) public {
+        // imageURI
+        string memory imageURI = svgToImageURI(svg);
+        // tokenURI
+        string memory tokenURI = formatTokenURI(imageURI);
+
+        _safeMint(msg.sender, tokenCounter);
+    }
+
+    function formatTokenURI(string memory imageURI) public pure return (string memory) {
+    string memory tokenURL = string.concat("data:application/json;base64,",
+        Base64.encode(
+        string memory json =  bytes(
+            string.concat(
+                '{"name":"SVG NFT", 
+                "description":"An NFT based SVG!",
+                "attributes":"",
+                "image":,"', imageURI, '"}')
+        )));
+        return tokenURI;
+    }
+
+    function svgToImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
+    {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        string memory imageURI = string.concat(baseURL, svgBase64Encoded);
+        return imageURI;
     }
 }
